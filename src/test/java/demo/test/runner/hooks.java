894e203ef180adc95537dd64067890fd47a673a8
cmaurys <christian.maury@rimac.com.pe>
2023-02-09 02:18:58 -0500

    upd

--- src/test/java/demo/test/runner/hooks.java
+++ src/test/java/demo/test/runner/hooks.java
@@ -22,17 +22,17 @@ public class hooks extends PageObject{
 		String v_environment = variables.getProperty("environment");
 		System.out.println(v_environment);
 
-		if (!v_environment.contains("local")) {
-			TestOutcome outcome = StepEventBus.getEventBus().getBaseStepListener().getCurrentTestOutcome();
-			System.out.println("SauceOnDemandSessionID="+RemoteDriver.of(getDriver()).getSessionId().toString()+ " job-name="+outcome.getCompleteName());
-			
-//			if(outcome.isSuccess()) {
-//				appiumDriver().executeScript("sauce:job-result=passed");
-//			}else {
-//				sendEmail(outcome);
-//				appiumDriver().executeScript("sauce:job-result=failed");
-//			}
-		}
+//		if (!v_environment.contains("local")) {
+//			TestOutcome outcome = StepEventBus.getEventBus().getBaseStepListener().getCurrentTestOutcome();
+//			System.out.println("SauceOnDemandSessionID="+RemoteDriver.of(getDriver()).getSessionId().toString()+ " job-name="+outcome.getCompleteName());
+//			
+////			if(outcome.isSuccess()) {
+////				appiumDriver().executeScript("sauce:job-result=passed");
+////			}else {
+////				sendEmail(outcome);
+////				appiumDriver().executeScript("sauce:job-result=failed");
+////			}
+//		}
 
 	}
 }
